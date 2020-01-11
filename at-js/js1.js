8cd36c31c5c7f48412020ca7c6d9058c1b3e034a
at-dieunguyen <dieu.nguyen@asiantech.vn>
2020-01-11 12:04:01 +0700

    fix js exercise

--- at-js/js1.js
+++ at-js/js1.js
@@ -1,6 +1,42 @@
-function sum(a,b){
-  if (a===b){
-    return 3*(a+b);
+//js1
+function sum(a, b) {
+  if (a === b) {
+    return 3 * (a + b);
   }
-    return a+b;
+  return a + b;
 }
+console.log(sum(1, 2))
+//js2
+function ex2(a) {
+  if (a > 19) {
+    return 3 * (a - 19);
+  }
+  return 19 - a;
+}
+console.log(22);
+//js3
+function divisionThree(str) {
+  var arr = [];
+  for (var i = 0; i <= 9; i++) {
+    var rep = str.replace("*", i);
+    if (parseInt(rep) % 3 === 0) {
+      rep = parseInt(rep);
+      arr.push(rep);
+    }
+  }
+  return arr;
+}
+console.log(divisionThree('19*'));
+//js4
+function divisionSix(str) {
+  var arr = [];
+  for (var i = 0; i <= 9; i++) {
+    var rep = str.replace("*", i);
+    if (parseInt(rep) % 6 === 0) {
+      rep = parseInt(rep);
+      arr.push(rep);
+    }
+  }
+  return arr;
+}
+console.log(divisionSix('19*'));
--- at-js/js2.js
+++
@@ -1,6 +0,0 @@
-function ex2(a){
-  if (a >19){
-    return 3*(a-19);
-  }
-  return 19-a;
-}
