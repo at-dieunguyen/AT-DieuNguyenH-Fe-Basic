d107819558ae4f2ca6f45fec941d16fe41e0f383
at-dieunguyen <dieu.nguyen@asiantech.vn>
2020-02-04 13:51:33 +0700

    update remove comment, style css

--- at-exOntap/comment.js
+++ at-exOntap/comment.js
@@ -9,51 +9,60 @@ function addComment() {
   comment = comment ? JSON.parse(comment) : [];
   comment.push(document.getElementById('comment-input').value);
   localStorage.setItem('comment', JSON.stringify(comment));
+  
 }
 function render() {
   var comment = JSON.parse(localStorage.getItem('comment'));
-  var row = document.getElementById('commented');
+  var div1 = document.getElementById('commented');
   for (var i = 0; i < comment.length; i++) {
-    var text = document.createElement('div');
-    text.classList = 'comment-row';
-    row.appendChild(text);
-    
-    
+    var div2 = document.createElement('div');
+    div2.classList = 'div2';
+    div1.appendChild(div2);
+    //div3
+    var div3 = document.createElement('div');
+    div3.classList = 'div3';
+    div2.appendChild(div3);
     //img
     var img = document.createElement('img');
     img.src = 'img/dieu.jpg';
-    text.appendChild(img);  
-    img.classList = 'img-avt';
-    //name
- 
+    img.classList = 'img-avatar';
+    div3.appendChild(img);
+    //div4
+    var div4 = document.createElement('div');
+    div4.classList = 'div4';
+    div3.appendChild(div4);
     //name
     var name = document.createElement('p');
-    name.innerHTML = 'Diệu: ';
-    name.classList = 'text-name';
-    text.appendChild(name);
+    name.innerHTML = 'Dieu: ';
+    div4.appendChild(name);
     //comment
-    var p = document.createElement('p');
-    p.innerHTML = comment[i];
-    p.classList = 'text-comment';
-    text.appendChild(p);
+    var cmt = document.createElement('p');
+    cmt.innerHTML = comment[i];
+    div4.appendChild(cmt);
     //button
     var btn = document.createElement('button');
-    btn.innerHTML = 'Remove';
+    div2.appendChild(btn);
+    btn.innerHTML = 'remove';
     btn.classList = 'btn-remove';
-    btn.setAttribute('data-id', 'index');
-    text.appendChild(btn);
-    // btn.addEventListener('click', deleteCmt);
   }
 }
+
 function deleteCmt() {
+
   var comment = localStorage.getItem('comment');
   var btnDel = document.getElementsByClassName('btn-remove');
+  comment = comment ? JSON.parse(comment) : [];
   for (var i = 0; i < btnDel.length; i++) {
-    btnDel[index].addEventListener('click', function () {
-      //comment = comment ? JSON.parse(comment) : [];
-        comment.splice(i, 1);
-        console.log(comment[index]);
-    })
+    btnDel[i].addEventListener('click', function (event) {
+      var remove = event.target.dataset.id;
+      comment.splice(remove, 1);
+      // console.log(comment);
+      localStorage.setItem('comment', JSON.stringify(comment));
+      // location.reload();
+      render();
+      location.reload();
+    });
   }
 }
 deleteCmt();
+
